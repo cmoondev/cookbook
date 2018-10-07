e687963c25ce1c892aa22d688330dd404d7886c0
not_a_cat <fancy.zanta@gmail.com>
2018-10-07 16:05:09 +0300

    JSON data store format

--- rendertable.js
+++ rendertable.js
@@ -446,21 +446,37 @@ function main() {
 		});
 	document.getElementById("btnReset").addEventListener("click", resetFields);
 	document.getElementById("btnSearch").addEventListener("click", refreshView);
-	// addDropdownToInput(document.getElementById("filterFEP"), evBaseName);
 	loadDataFromTables();
-	refreshView();
 }
 
 function loadDataFromTables() { //displayData generation
-	for( var food_index = 0; food_index < gl_food.length; food_index++) {
-		if( gl_food[food_index][1].length > 0 ) {
-			for (var j = 0; j < gl_fepmod.length; j++) {
-				if ( gl_fepmod[j][0] == gl_food[food_index][0] ) {
-					displayData.push([gl_fepmod[j][0], gl_fepmod[j][1], gl_fepmod[j][2], gl_food[food_index][3], gl_food[food_index][4], false]);
+	//load data from JSON
+	var divPageStats = document.getElementById("pageStats");
+	divPageStats.innerHTML = "Loading data...";
+
+	var xmlHttpR = new XMLHttpRequest();
+	xmlHttpR.onreadystatechange = function() {
+		if (this.readyState === 4 && this.status == 200) {
+			var importJSON = JSON.parse(this.responseText);
+			var food = importJSON.gl_food;
+			var ifep = importJSON.gl_fepmod;
+
+			for( var food_index = 0; food_index < food.length; food_index++) {
+				if( food[food_index][1].length > 0 ) { //if food has variable ingredients cycle through all FEP mods records
+					for (var j = 0; j < ifep.length; j++) {
+						if ( ifep[j][0] == food[food_index][0] ) {
+							displayData.push([ifep[j][0], ifep[j][1], ifep[j][2], food[food_index][3], food[food_index][4], false]);
+							ifep.splice(j, 1); //remove found FEP mods record to exclude it from future cycles
+						}
+					}
+				} else {
+					displayData.push([food[food_index][0], food[food_index][1], food[food_index][2], food[food_index][3], food[food_index][4], false]);
 				}
 			}
-		} else {
-			displayData.push([gl_food[food_index][0], gl_food[food_index][1], gl_food[food_index][2], gl_food[food_index][3], gl_food[food_index][4], false]);
+			
+			refreshView();
 		}
 	}
+	xmlHttpR.open("GET", "tables.json", true);
+	xmlHttpR.send();
 }
